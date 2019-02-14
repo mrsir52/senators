7580a869abd45abbc7082e649b1ff06688723c88
Manny <coachmannytw@gmail.com>
2019-02-14 12:20:40 -0700

    simple fetch

--- src/App.js
+++ src/App.js
@@ -3,23 +3,40 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+  //This will set the state, by default we are setting senators to an empty Array
+  state = {
+    senators: []
+  };
+
+  //see article on componentDidMount vs componentWillMount 
+  //https://medium.com/coffee-and-codes/componentdidmount-v-s-componnetwillmount-react-47f4f631276c
+  componentDidMount () {
+    //This will fetch the data
+    fetch('https://raw.githubusercontent.com/CivilServiceUSA/us-senate/master/us-senate/data/us-senate.json')
+    //this turns the returned data in JSON
+    .then(response =>  {
+      return response.json();
+    })
+    //This will set state, you can keep the console log the data in the console or remove it form this code
+    //It may be a good idea to keep it for reference until you have displayed that data that you need
+    .then(data => {
+      console.log("data:", data)
+      this.setState({senators: data})
+    });
+  }
+  render() 
+  {    
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <h1>hello</h1>
+      {/* This is how you would use map to loop over the data to display what you want */}
+       {this.state.senators.map((senator)=> (
+       <div>
+         <h1>Name: {senator.name}</h1>
+         <h2>State: {senator.state_name}</h2>
+         <h3>Gender: {senator.gender}</h3>
+         </div>))}
+         {/* end of map */}
       </div>
     );
   }
