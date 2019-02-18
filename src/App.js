7c069208ad6d9d63ed11bae0bc4658aed4e3c09f
Manny <coachmannytw@gmail.com>
2019-02-17 22:18:17 -0700

    add form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { link } from "fs";
+import Form from "./components/Form";
 
 class App extends Component {
   //This will set the state, by default we are setting senators to an empty Array
@@ -8,40 +10,51 @@ class App extends Component {
     senators: []
   };
 
-  //see article on componentDidMount vs componentWillMount 
+  //see article on componentDidMount vs componentWillMount
   //https://medium.com/coffee-and-codes/componentdidmount-v-s-componnetwillmount-react-47f4f631276c
-  componentDidMount () {
+  componentDidMount() {
     //This will fetch the data
-    fetch('https://raw.githubusercontent.com/CivilServiceUSA/us-senate/master/us-senate/data/us-senate.json')
-    //this turns the returned data in JSON
-    .then(response =>  {
-      return response.json();
-    })
-    //This will set state, you can keep the console log the data in the console or remove it form this code
-    //It may be a good idea to keep it for reference until you have displayed that data that you need
-    .then(data => {
-      console.log("data:", data)
-      this.setState({senators: data})
-    });
+    fetch(
+      "https://raw.githubusercontent.com/CivilServiceUSA/us-senate/master/us-senate/data/us-senate.json"
+    )
+      //this turns the returned data in JSON
+      .then(response => {
+        return response.json();
+      })
+      //This will set state, you can keep the console log the data in the console or remove it form this code
+      //It may be a good idea to keep it for reference until you have displayed that data that you need
+      .then(data => {
+        console.log("data:", data);
+        this.setState({ senators: data });
+      });
   }
 
+  render() {
+    const senatorsList = this.state.senators.map(senator => {
+      return (
+        <div>
+          <h5>{senator.name}</h5>
+          <h6>{senator.state_name}</h6>
+        </div>
+      );
+    });
 
-  render() 
-  {    
     return (
       <div className="App">
-       <h1>hello</h1>
-      {/* This is how you could use map to loop over the data to display what you want */}
-      {/* sort is a mutable function that changes the Array */}
-       {this.state.senators.sort((a, b) => a.itemM > b.itemM)
+        <h1>hello</h1>
+        <Form />
+        {senatorsList}
+        {/* This is how you could use map to loop over the data to display what you want */}
+        {/* sort is a mutable function that changes the Array */}
+        {/* {this.state.senators.sort((a, b) => a.itemM > b.itemM)
        .map((senator)=> (
        <div>
          <h1>Name: {senator.name}</h1>
          <h2>State: {senator.state_name}</h2>
          <button>Gender: {senator.gender}</button>
-         </div>))}
-        
-         {/* end of map */}
+         </div>))} */}
+
+        {/* end of map */}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import data from './myData/data'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+ReactDOM.render(<App data={data} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
